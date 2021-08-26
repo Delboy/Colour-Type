311a0e548933eb67166227513119cefec2eb1cc2
Delboy <Derek.degoey@hotmail.co.uk>
2021-08-26 17:22:24 +0000

    add generateQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
+document.addEventListener('keydown', function(event) {
+    if(event.key ==='Enter') {
+        generateQuestion();
+    }
+})
+
 function runGame(){
-
-}
-
-function gameTImer(){
+    generateQuestion();
     
 }
 
-function generateQuestion(){
+function gameTImer(){
 
+}
+
+function generateQuestion(){
+    let conditions = ['word', 'colour'];
+    let colours = ['Red', 'Green', 'Blue', 'Yellow', 'Orange', 'Purple', 'Pink', 'Black'];
+
+    let randomCondition = Math.floor(Math.random() * conditions.length);
+    let randomColour = Math.floor(Math.random() * colours.length);
+    let randomStyle = Math.floor(Math.random() * colours.length);
+
+    let condition = document.getElementById('cond1');
+    let colour = document.getElementById('cond2');
+
+    condition.textContent = conditions[randomCondition];
+    colour.textContent = colours[randomColour];
+    colour.style.color = colours[randomStyle];
 }
 
 function checkQuestion(){
